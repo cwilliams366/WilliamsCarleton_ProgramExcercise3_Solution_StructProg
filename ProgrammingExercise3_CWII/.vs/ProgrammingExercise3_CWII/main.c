71d79109f2ecf9b346ebfaae941419efb3471656
Carleton Williams <carleton.williams1@bison.howard.edu>
2024-04-04 13:59:44 -0400

    Created the .c file for the assignment with a testing console print out.

---
+++ ProgrammingExercise3_CWII/.vs/ProgrammingExercise3_CWII/main.c
@@ -0,0 +1,15 @@
+/*
+Name: Carleton Williams II
+Student ID: @30328817
+Date: April 2nd, 2024
+Course: Structures of Programming
+Professor: Dr. Linwei Niu
+Homework 3: Programming Exercise 3
+Assignment: Write and test the code to implement the state diagram of Problem 1.
+*/
+#include <stdio.h>
+int main()
+{
+    printf("Testing the state diagram of Problem 1");
+	return 0;
+}
